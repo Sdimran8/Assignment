a2856ed2f191c444ea7e9c943b6e966cd4a17a95
sdimran8 <imransyed.marolix@gmail.com>
2023-07-11 21:19:04 +0530

    Third comment

---
+++ strips.py
@@ -0,0 +1,40 @@
+# n = '   python software   '
+# print(n.lstrip())
+
+# n = '   programme foundation'
+# print(n.lstrip())
+
+# n = ' ravi   ram   '
+# print(n.lstrip())
+
+# k = '  hallow paterns'
+# print(k.lstrip())
+
+# p ='   Terminology  '
+# print(p.rstrip())
+
+# j='output  '
+# print(j.rstrip())
+
+# l = 'hello world'
+# print(l.find('l'))
+
+# j = 'hyderabad'
+# print(j.find('rok'))
+
+# h = 'America'
+# print(h.find('c'))
+
+# d = 'decathlon'
+# print(d.index('row'))
+
+# d = "Ramu is a boy & ramu studies well"
+# print(d.rfind('ramu'))
+
+# w='worldclass player'
+# print(w.rfind('u'))
+
+# if w==-1:
+#     pass
+# print('Thank you')
+
