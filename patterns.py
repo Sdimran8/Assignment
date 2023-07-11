7a6616e56b3d5138cc8a9910b3df44d5e11c6f16
sdimran8 <imransyed.marolix@gmail.com>
2023-07-11 14:49:56 +0530

    second commit

---
+++ patterns.py
@@ -0,0 +1,134 @@
+# n = int(input("enter your input"))
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()    
+
+# n = int(input("enter num of rows"))
+
+# for i in range(n):
+#     for j in range(n):
+#         if j==0 or i==n-1 or i==j:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(n):
+#         if i==0 or j==0 or i+j==n-1:
+#             print('*',end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()                       
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(n):
+#         if i==0 or j==n-1 or i==j:
+#             print('*',end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(n):
+#         if i==n-1 or j==n-1 or i+j==n-1:
+#             print(i+1,end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(n-i):
+#         print(' ',end=' ')
+#     for j in range(i+1):
+#         print(i+1,end=" ") 
+#     print()       
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print(' ',end='')
+#     for j in range(i+1):
+#         print('*',end=" ")    
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n-1,-1,-1):
+#     for j in range(n-i-1):
+#         print(' ',end='')
+#     for j in range(i+1):
+#         print('*',end=" ")    
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(i):
+#         print(' ',end=' ')
+#     for j in range(n-i):
+#         print(j+1,end=" ")    
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(i):
+#         print(' ',end=' ')
+#     for j in range(n-i):
+#         print(i+1,end=" ")    
+#     print() 
+    
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(i):
+#         print(' ',end=' ')
+#     for j in range(n-i):
+#         print(n-j,end=" ")    
+#     print()     
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(n-i):
+#         print(n-i,end=" ")    
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(i):
+#         print(' ',end=' ')
+#     for j in range(n-i):
+#         print(n-i,end=" ")    
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(n-i):
+#         print(' ',end=' ')
+#     for j in range(i+1):
+#         print(n-j,end=" ")    
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(n-i):
+#         print(n-j,end=" ")    
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n):
+#     for j in range(i+1):
+#         print(' ',end=' ')
+#     for j in range(n-i):
+#         print(j+1,end=" ")    
+#     print() 
+
+# n = int(input('enter input'))
+# for i in range(n-1,-1,-1):
+#     for j in range(n-i-1):
+#         print(' ',end='')
+#     for j in range(i+1):
+#         print(j+1,end=" ")    
+#     print() 
