5369cb4b827cb5fede7586e619b21f969279483d
sdimran8 <imransyed.marolix@gmail.com>
2023-07-11 21:12:06 +0530

    Third comment

---
+++ break & continue.py
@@ -0,0 +1,67 @@
+# n = "python"
+
+# for i in n:
+#     if i == "h":
+#         break
+#     print(i)
+
+# i = 1
+
+# while i <=10:
+#     print("8 *" ,(i), "=" ,8 * i)
+
+#     if i == 5:
+#         break
+#     i +=1
+
+# country = ["America","india","england","canada","Australia"]
+# for i in country:
+#     if i =="england":
+#         break
+#     print(i)
+
+# for i in range(9):
+#     if i >=7:
+#         break
+#     print(i)
+
+# n = ("hi these is my,first programm")
+
+# for i in n:
+#     if i == ",":
+#         break
+#     print(i)
+
+# n = "97860067"
+# for i in n:
+#     if i == "0":
+#         continue
+#     print(i)
+
+# for i in range(10):
+#     if i%2 != 0:
+#         continue
+#     print(i)    
+
+# i = 0
+# while i <=10:
+#     if i ==5:
+#         continue
+#     print(i)
+#     i = i+1
+
+
+# n = [2,-3,4,-5,0,8]
+# for i in n:
+#     if i <=0:
+#         continue
+#     print(i)
+
+# n = "go4o4d mo4rni4n4g4"
+# for i in n:
+#     if i=='4':
+#         continue
+#     print(i)
+
+s="sunny bhai"
+print(s[-4:3:-1])
---
+++ indexing.py
@@ -0,0 +1,69 @@
+# n = "hi ramu"
+# print(n[:4])
+
+# n="sunny"
+# print(n[2:])
+
+# s = "suresh"
+# print(s[:])
+
+# x = "Raju"
+# print(x[::-1])
+
+# v = "prabhas"
+# print(v[:-4])
+
+# n ="hello good morning"
+# print(n[2:9:2])
+
+# l = "coderunner"
+# print(l[-1:-10:-3])
+
+# s = "software developer"
+# print(s[-4:-14:-2])
+
+# p = "python programme"
+# print(p[-12:-3:2])
+
+# j = "javascript"
+# print(j[2:9:2])
+
+# k='Full stack developer'
+# print(k[2:9:-2])
+
+# p = "python programming"
+# print(p[-3:-12:2])
+
+# u = 'supernovas'
+# print(u[-2:-9:-3])
+
+# n = 'recruting'
+# print(n[2:8:-2])
+
+# k = "world cup"
+# print(k[-8:-2])
+
+# r = 'democracy'
+# print(r[-1:-6:-2])
+
+# f='fast & furious'
+# print(f[-3:-10:-2])
+
+# r= 'break & continue'
+# print(r[-5:-10:-3])
+
+# p = 'terminology'
+# print(p[1:10:4])
+
+# g="good evening"
+# print(g[-3:-11:-1])
+
+# d='denmark'
+# print(d[-7:-13:-2])
+
+# l='ui developer'
+# print(l[-2:-5:-2])
+
+# b = 'boosting economy'
+# print(b[-2:-9:-3])
+
