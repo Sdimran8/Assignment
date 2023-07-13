1c7f02307d81512cfff14a2c9550fdf8459a7d5a
sdimran8 <imransyed.marolix@gmail.com>
2023-07-13 10:26:26 +0530

    fourth commit

---
+++ string methods.py
@@ -0,0 +1,35 @@
+# s = "python is easy language easy is python"
+# print(s.count('easy'))
+
+# g = "java is easy"
+# print(g.replace('java','python'))
+
+# p = 'python/is/ programming/ language'
+# print(p.split('/'))
+
+# h = '97978'
+# print(h.isdigit())
+
+# h = '9797d8'
+# print(h.isdigit())
+
+# k='kompali@@@@'
+# print(k.strip('@'))
+
+# j = 'tollywood'
+# print(j.startswith('t'))
+
+# l = 'secunderabad'
+# print(l.startswith('a'))
+
+# k = 'ANDHRA pradesh'
+# print(k.upper())
+
+# l = "TelAnGana"
+# print(l.lower())
+
+# k = 'ramu is a good boy'
+# print(k.replace('ramu','Raju'))
+
+# d = '9876h54321'
+# print(d.isdigit())
