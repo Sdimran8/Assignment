f3a8120d76175c4e0a024db0cb575fb60e839c32
sdimran8 <imransyed.marolix@gmail.com>
2023-07-20 17:43:33 +0530

    fivth commit

---
+++ Data structures.py
@@ -0,0 +1,33 @@
+                                                  #DATA STRUCTURES
+
+#LIST: Mutuable []
+
+#1.Lists are mutable, meaning that after creation, we can modify their elements.
+#2.It consists of data separated by commas inside square braces-[].
+#3.Lists allow duplicate elements.
+#4.We can concatenate lists using + and repeat elements using * operators.
+#5.We use indexing to access the list elements (0 to length - 1), and negative indexing is allowed too (-1 to -length).
+
+#TUPLE : Immutuable ()
+
+#1. Tuples allow duplicate elements like lists.
+#2. We use indexing to access the tuple elements (0 to length - 1), and negative indexing is allowed too (-1 to -length).
+#3. Creating a tuple without parenthesis is called "Tuple packing" and to access a tuple, we can also use "Tuple unpacking".
+#4. Iterating through a tuple is much faster than iterating through a list.
+#5. As a tuple is immutable, it can be used as a key in dictionaries.
+
+#SETS : UNIQUE ELEMENTS {} UNORDED
+
+#1 : It is a mutable data type and is iterable
+#2 : We can add or delete elements from a set, but we can't perform slicing or indexing operations on a set.
+#3 : If more than one element is placed in an index, the values are arranged in a linked list to that index position.
+#4 : Every index position acts as a key, and the members/ values in the index are stored as values inside a dictionary.
+#5 : A set won't allow mutable items as its elements like lists:
+
+# DICTIONARY {key:Value} pairs UNORDERD
+
+#1 : Keys identify values. Hence, a key has to be unique and immutable. Only immutable data types are accepted to be keys.
+#2 : Any data type can be used to make a value as it is the data.
+#3 : There cannot be any duplicate keys, but duplicate values are allowed.
+#4 : We can also use dictionary comprehension to create a dictionary like list comprehension.
+#5 : The dictionary's keys are like the Roll numbers of students, and the values are the students' names.
--- string methods.py
+++ string methods.py
@@ -33,3 +33,128 @@
 
 # d = '9876h54321'
 # print(d.isdigit())
+
+# r=input(' enter the string')
+# k = r.split()
+# l=list(reversed(k))
+# print(' '.join(l))
+
+# name = 'ramesh'
+# age = 20
+# print('hi {} how are you,my age is {}'.format(name,age))
+
+# h = 'ramu'
+# print(h.title())
+
+# k = 'hyderabad'
+# print(k.capitalize())
+
+# n = 'aabgfcchdnneff'
+# k =[]
+# for i in n:
+#     if i not in k:
+#         k.append(i)
+# o =''.join(k) 
+# print(o)
+
+#title
+
+# k = "programer"
+# print(k.title())
+
+# text = 'My favorite number is 8.'
+# print(text.title())
+
+# g = 'ram like white color'
+# print(g.title())
+
+# d = 'do you have pen'
+# print(d.title())
+
+# w = 'world class students'
+# print(w.title())
+
+#SWAPCASE :
+
+# p = 'lApToP'
+# print(p.swapcase())
+
+# h = 'dAtA sTrUcTurE'
+# print(h.swapcase())
+
+# a = 'TraIN'
+# print(a.swapcase())
+
+# f = 'HitEcH CiTy'
+# print(f.swapcase())
+
+# x = 'sUper MAn'
+# print(x.swapcase())
+
+#JOIN
+
+# text = ['Python', 'is', 'a', 'fun', 'programming', 'language']
+# print(' '.join(text))
+
+# num = ['1','2','4','5']
+# print(' '.join(num))
+
+# l = ['how','are','you']
+# print(' '.join(l))
+
+# h = ['telugu','hindi','english']
+# print(' '.join(h))
+
+# g = ['goods','and','services']
+# print(' '.join(g))
+
+#isupper
+
+# k = 'upper'
+# print(k.isupper())
+
+# l = 'LOWER'
+# print(l.isupper())
+
+# g = 'GRAND FATHER'
+# print(g.isupper())
+
+# f = 'france'
+# print(f.isupper())
+
+# d = 'INDIA'
+# print(d.isupper())
+
+#islower()
+
+# h = 'hockey'
+# print(h.islower())
+
+# o = 'AND'
+# print(o.islower())
+
+# h = 'umbrella'
+# print(h.islower())
+
+# a = 'USA'
+# print(a.islower())
+
+# d = 'danger'
+# print(d.islower())
+
+#isalpha()
+
+# name = "Monica"
+# print(name.isalpha())
+
+# name = "Monica Geller"
+# print(name.isalpha())
+
+# name = "Mo3nicaGell22er"
+# print(name.isalpha())
+
+# f = 'imran8'
+# print(f.isalpha())
+
+# s = 'substitute'
+# print(s.isalpha())
